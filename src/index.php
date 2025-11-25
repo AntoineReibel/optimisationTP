69d61aa40d1f1f0f9eb2c06575d1d8a47014ec75
AntoineReibel <124444455+AntoineReibel@users.noreply.github.com>
2025-11-25 08:59:26 +0100

    Deplacement des fichiers (#5)

--- src/index.php
+++ src/index.php
@@ -1,14 +1,16 @@
+<?php
+session_start(); ?>
+
 <html>
 <meta http-equiv="Content-Type"'.' content="text/html; charset=utf8"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
 <body>
 <?php
-session_start();
 	if(isset($_POST['ac'])){
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
+        $servername = "db";
+        $username = "root";
+        $password = "";
 
 		$conn = new mysqli($servername, $username, $password);
 
@@ -33,9 +35,9 @@ session_start();
 	}
 
 	if(isset($_POST['delc'])){
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
+        $servername = "db";
+        $username = "root";
+        $password = "";
 
 		$conn = new mysqli($servername, $username, $password);
 
